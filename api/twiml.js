d0dc587aa3b731182110a5510dbf8d2e5e22c08c
pedritohmb18012013-ux <pedritohmb18012013@gmail.com>
2026-09-16 20:46:07 -0300

    Update twiml.js

--- api/twiml.js
+++ api/twiml.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.type("text/xml").send("<Response><Say>teste</Say><Hangup/></Response>");
+  const texto = req.query.texto || "";
+
+  const seguro = texto
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+
+  res.type("text/xml").send(`
+    <Response>
+      <Say language="pt-BR">${seguro}</Say>
+      <Hangup/>
+    </Response>
+  `);
 }   
