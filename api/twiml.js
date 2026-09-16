9dfeabe53b6fe47a16152200bcde6408d3d65ec6
pedritohmb18012013-ux <pedritohmb18012013@gmail.com>
2026-09-16 20:11:19 -0300

    Sanitize text input for XML response
    Sanitize input text to prevent XML breaking characters.

--- api/twiml.js
+++ api/twiml.js
@@ -1,8 +1,16 @@
 export default function handler(req, res) {
   const texto = req.query.texto || "";
+
+  // Remove caracteres que quebram o XML
+  const seguro = texto
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+
   res.type("text/xml").send(`
     <Response>
-      <Say language="pt-BR">${texto}</Say>
+      <Say language="pt-BR">${seguro}</Say>
       <Hangup/>
     </Response>
   `);
