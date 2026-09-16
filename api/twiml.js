7b8cc08e6c25069a67ade1167eb9160d7d9fd8a9
pedritohmb18012013-ux <pedritohmb18012013@gmail.com>
2026-09-16 20:20:05 -0300

    Simplify TwiML response in handler function

--- api/twiml.js
+++ api/twiml.js
@@ -1,17 +1,3 @@
 export default function handler(req, res) {
-  const texto = req.query.texto || "";
-
-  // Remove caracteres que quebram o XML
-  const seguro = texto
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
-
-  res.type("text/xml").send(`
-    <Response>
-      <Say language="pt-BR">${seguro}</Say>
-      <Hangup/>
-    </Response>
-  `);
+  res.type("text/xml").send("<Response><Say>teste</Say><Hangup/></Response>");
 }   
